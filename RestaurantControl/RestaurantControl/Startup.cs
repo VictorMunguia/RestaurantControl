b94fb301291546c26a2285245bd531380198a374
VictorMunguia <victor00171@hotmail.com>
2018-05-21 11:04:25 -0500

    First commit, the navbar had been moved to partial view.

---
+++ RestaurantControl/RestaurantControl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantControl.Startup))]
+namespace RestaurantControl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
