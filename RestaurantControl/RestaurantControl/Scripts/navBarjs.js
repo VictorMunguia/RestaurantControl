f1df16224ee8ccfd9c85ff306fce687812ea9611
VictorMunguia <victor00171@hotmail.com>
2018-05-28 20:19:44 -0500

    adding effects in the navBar and putting the margin of the video in 0, created a new layout for the rest of the views

---
+++ RestaurantControl/RestaurantControl/Scripts/navBarjs.js
@@ -0,0 +1,7 @@
+﻿$(window).scroll(function () {
+    if ($("#menu").offset().top > 56) {
+        $("#menu").addClass("colorCambio");
+    } else {
+        $("#menu").removeClass("colorCambio")
+    }
+});
